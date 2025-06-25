41a080ff96a5849528acaacb9540968fc27a96fd
hebaak19 <hebaalkathiri9@gmail.com>
2025-06-25 17:51:08 +0300

    add statics file

---
+++ src/data/statics.ts
@@ -0,0 +1,9 @@
+import User from "../../public/images/user.png";
+import Location from "../../public/images/location.png";
+import Server from "../../public/images/storage.png";
+
+export const statics = [
+  { image: User, number: "90+", name: "Users" },
+  { image: Location, number: "30+", name: "Location" },
+  { image: Server, number: "50+", name: "Severs" },
+];
